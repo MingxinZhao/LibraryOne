3d9b9ef6db0f5f9c1f1ae3cf386b7438c3ac10ca
admin <13466759977@163.com>
2017-09-27 19:11:18 +0800

    first commit

---
+++ libraryone/src/main/java/helloworld/hello/com/libraryone/ActivityUtils.java
@@ -0,0 +1,102 @@
+package helloworld.hello.com.libraryone;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+
+
+/**
+ * Activity跳转工具管理
+ *
+ */
+public class ActivityUtils {
+	/**
+	 * 带参数的Activity跳转：
+	 * <p> 参数都封装在bundle里面
+	 * @param activity 当前的activity
+	 * @param targetActivity 目标activity
+	 * @param bundle 参数
+	 */
+	public static void switchTo(Activity activity, Class<? extends Activity> targetActivity, Bundle bundle){
+		Intent intent =new Intent(activity,targetActivity);
+		intent.putExtras(bundle);
+		switchTo(activity, intent);
+	}
+	/**
+	 * 不带参数的Activity跳转：
+	 * <p> 参数都封装在bundle里面
+	 * @param activity 当前的activity
+	 * @param class1 目标activity
+	 */
+	public static void switchTo(Activity activity, Class<? extends Activity> class1){
+		Intent intent =new Intent(activity,class1);
+		switchTo(activity, intent);
+	}
+	/**
+	 * 不带参数的Activity跳转：
+	 * <p> 参数都封装在bundle里面
+	 * @param activity 当前的activity
+	 * @param targetActivity 目标activity
+	 */
+	public static void switchTo(Activity activity, Class<? extends Activity> targetActivity, boolean isJump){
+		if(isJump){
+			Intent intent =new Intent(activity,targetActivity);
+			switchTo(activity, intent);
+		}
+	}
+	/**
+	 * 跳转到指定的activity，并需要返回结果
+	 * @param activity
+	 * @param targetActivity
+	 * @param requestCode
+	 */
+	public static void switchToForResult(Activity activity, Class<? extends Activity> targetActivity, int requestCode){
+		Intent intent =new Intent(activity,targetActivity);
+		switchToForResult(activity, intent, requestCode);
+	}
+	/**
+	 *  跳转到指定的activity，并需要返回结果
+	 * @param activity
+	 * @param targetActivity
+	 * @param bundle
+	 * @param requestCode
+	 */
+	public static void switchToForResult(Activity activity, Class<? extends Activity> targetActivity, Bundle bundle, int requestCode){
+		Intent intent =new Intent(activity,targetActivity);
+		intent.putExtras(bundle);
+		switchToForResult(activity, intent, requestCode);
+	}
+
+	/**
+	 * 相机获取图片
+	 * @param activity
+	 * @param uri
+	 * @param requestCode
+	 */
+	public static void switchToCapturePhoto(Activity activity, Uri uri, int requestCode) {
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+		activity.startActivityForResult(intent, requestCode);
+	}
+	/**
+	 *  跳转到指定的activity，并需要返回结果
+	 * @param activity
+	 * @param intent
+	 * @param requestCode
+	 */
+	public static void switchToForResult(Activity activity, Intent intent, int requestCode){
+		activity.startActivityForResult(intent, requestCode);
+	}
+	/**
+	 * 根据给定的Intent进行Activity跳转
+	 * @param activity
+	 * @param intent
+	 */
+	public static void switchTo(Activity activity, Intent intent){
+		activity.startActivity(intent);
+	}
+
+}
